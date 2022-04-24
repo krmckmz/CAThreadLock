c87c4683f9ea5c313f5e5c906da769bd88702ec7
Kerem Cokmez <kerem.cokmez@medyasoft.com.tr>
2022-04-24 14:37:50 +0300

    Add project files.

---
+++ CAThreadLock/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //Race Condition 
+        //Critical Section
+        Thread t1 = new Thread();
+        Thread t2 = new Thread();
+    }
+}
+
+class Account
+{
+    decimal balance;
+
+    //Bu field anahtar görevi görür.
+    private Object thisLock = new Object();
+
+    public void WithDraw(decimal amount)
+    {
+        lock (thisLock)
+        {
+            if (amount > balance)
+            {
+                throw new Exception("Insufficient funds");
+            }
+
+            balance -= amount;
+        }
+    }
+}
